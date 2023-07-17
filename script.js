635b9206a2bd469ec7f4e3e616276e4227bb5412
abxzok379 <129567055+abxzok379@users.noreply.github.com>
2023-07-16 20:04:06 -0400

    Mise en ligne

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function calculateurTaxes(){
+var montant = Number(document.querySelector("#montantUtilisateur").value);
+var TPS = 0.05;
+var TVQ = 0.0975;
+var montantAfterTPS = (montant*TPS)+montant;
+var montantAfterTVQ = (montantAfterTPS*TVQ)+montantAfterTPS;
+document.querySelector("#valeur01").innerHTML = montant + "$";
+document.querySelector("#valeur02").innerHTML = Math.round(montantAfterTVQ) +"$";
+
+}
